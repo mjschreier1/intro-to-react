91fb5de240845b04e6299364ee6ca60516298e25
Michael Schreier <mjschreier1@gmail.com>
2018-06-01 18:39:58 -0600

    passed data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
+import AddProject from './Components/AddProject';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			projects: []
+		}
+	}
+
+	componentWillMount() {
+		this.setState({
+			projects: [
+				{
+					title: 'Business Website',
+					category: 'Web Design'
+				},
+				{
+					title: 'Social App',
+					category: 'Mobile Development'
+				},
+				{
+					title: 'E-Commerce Shopping Cart',
+					category: 'Web Design'
+				} 
+			]	
+		})
+	}
+
+	handleAddProject(project) {
+		let projects = this.state.projects;
+		projects.push(project);
+		this.setState({ project: projects });
+	}
+
   render() {
     return (
       <div className="App">
-        My App
+				<AddProject addProject={ this.handleAddProject.bind(this) } />
+				<Projects projects={ this.state.projects } />
       </div>
     );
   }
---
+++ src/Components/AddProject.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+	constructor() {
+		super();
+		this.state = {
+			newProject: {}
+		}
+	}
+
+	static defaultProps = {
+		categories: ['Web Design', 'Web Development', 'Mobile Development']
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		if (this.refs.title.value === '') {
+			alert('Title is required!')
+		} else {
+			this.setState({ newProject: 
+				{
+					title: this.refs.title.value,
+					category: this.refs.category.value
+				} 
+			}, () => {
+				this.props.addProject(this.state.newProject);
+			})
+		}
+	}
+
+	render() {
+		let categoryOptions = this.props.categories.map(category => {
+			return <option key={ category } value={ category }>{ category }</option>
+		})
+
+		return (
+			<div>
+				<h3>Add Project</h3>
+				<form onSubmit={ this.handleSubmit.bind(this) }>
+					<div>
+						<label htmlFor="title">Title</label><br />
+						<input type="text" ref="title" />
+					</div>
+					<div>
+						<label htmlFor="category">Category</label><br />
+						<select ref="category">
+							{ categoryOptions }
+						</select>
+					</div>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+	}
+}
+
+export default AddProject;
